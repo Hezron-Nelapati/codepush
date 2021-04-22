12860b23c5ede7eb2836f391b63774ac3cf7cc85
Hezron-Nelapati <nelapatihezron83@gmail.com>
2021-04-22 15:11:55 +0530

    .

--- android/app/src/main/java/com/codepush/MainApplication.java
+++ android/app/src/main/java/com/codepush/MainApplication.java
@@ -1,5 +1,6 @@
 package com.codepush;
 
+import com.microsoft.codepush.react.CodePush;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -32,6 +33,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
       };
 
